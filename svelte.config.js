19b9215cb93f1a68a9b149cd4a1a4dc502e971ec
shr00m335 <zcabswu@ucl.ac.uk>
2025-09-15 18:25:00 +0100

    Updated config path

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/wifi-3d-qr-code-generator" : ""
+			base: process.env.NODE_ENV === "production" ? "/wifi-3d-qr-code-model-generator" : ""
 		},
 		prerender: {
 			handleMissingId: "warn"
