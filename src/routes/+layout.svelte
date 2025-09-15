be0ccd1e08e445d2c5f2f91b3ac473e20f79477a
shr00m335 <zcabswu@ucl.ac.uk>
2025-09-15 18:02:28 +0100

    Make the site static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import favicon from "$lib/assets/favicon.svg";
 	import "../app.css";
-
-	export const prerender = true;
-
-	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<slot />
